86714d82222a1074755afc4e54bc7ffdc236b27b
Stjepan Bakrac <S.Bakrac@impeo.org>
2017-08-01 08:39:50 +0200

    Refactored exception class

--- Refer.Tests/InvalidReferenceExceptionTest.cs
+++ Refer.Tests/InvalidReferenceExceptionTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Refer.Tests
@@ -8,17 +9,11 @@ namespace Refer.Tests
         [TestMethod]
         public void InvalidReferenceException_Constructor()
         {
-            var ex1 = new InvalidReferenceException();
-            Assert.AreEqual($"Exception of type '{ex1.GetType().FullName}' was thrown.", ex1.Message);
-            Assert.AreEqual(null, ex1.InnerException);
+            var ex = new Exception();
 
-            var ex2 = new InvalidReferenceException("test");
-            Assert.AreEqual("test", ex2.Message);
-            Assert.AreEqual(null, ex2.InnerException);
-
-            var ex3 = new InvalidReferenceException("message", ex2);
-            Assert.AreEqual("message", ex3.Message);
-            Assert.AreEqual(ex2, ex3.InnerException);
+            var test = new InvalidReferenceException("message", ex);
+            Assert.AreEqual("message", test.Message);
+            Assert.AreEqual(ex, test.InnerException);
         }
     }
 }
--- Refer/InvalidReferenceException.cs
+++ Refer/InvalidReferenceException.cs
@@ -7,14 +7,6 @@ namespace Refer
     /// </summary>
     public class InvalidReferenceException : Exception
     {
-        internal InvalidReferenceException()
-        {
-        }
-
-        internal InvalidReferenceException(String message) : base(message)
-        {
-        }
-
         internal InvalidReferenceException(String message, Exception innerException) : base(message, innerException)
         {
         }
